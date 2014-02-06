a6eecc14f7a544ad8a23f210e9fc12e6c77408b0
LoriMinor <lorim808@gmail.com>
2014-02-06 11:34:23 -0500

    Added implicit intent to invoke ImageGallery.

--- src/com/plantplaces/plantplacesmobile14ss/GPSAPlantActivity.java
+++ src/com/plantplaces/plantplacesmobile14ss/GPSAPlantActivity.java
@@ -1,16 +1,28 @@
 package com.plantplaces.plantplacesmobile14ss;
 
+import com.plantplaces.dto.Plant;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GPSAPlantActivity extends Activity {
 	
+	private static final int CAMERA_RESULT = 5;
 	// declare a variable that will hold a reference to the EditText component on the screen.
 	EditText description;
+	private TextView txtSelectedPlant;
+	private Plant plant;
+	private Bitmap plantImage;
+	private ImageView imgPlant;
+	
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,12 @@ public class GPSAPlantActivity extends Activity {
         setContentView(R.layout.activity_gps_plants);
         
         description = (EditText) findViewById(R.id.edtDescription);
+        
+        //the label that contains the description of the selected plant
+        txtSelectedPlant = (TextView) findViewById(R.id.txtSelectedPlant);
+        
+        //reference to image view that will display the photo
+        imgPlant = (ImageView) findViewById(R.id.imgPlant);
     }
 
 
@@ -33,7 +51,42 @@ public class GPSAPlantActivity extends Activity {
     	Intent searchIntent = new Intent(this, AdvancedSearchActivity.class);
     	
     	//start the activity
-    	startActivity(searchIntent);
+    	startActivityForResult(searchIntent, AdvancedSearchActivity.PLANT_RESULTS);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	// TODO Auto-generated method stub
+    	super.onActivityResult(requestCode, resultCode, data);
+
+    	if (resultCode == RESULT_OK) {
+    		// only do this work if we received a good result.
+    		if (requestCode == AdvancedSearchActivity.PLANT_RESULTS) {
+    			// change the label of the text view to be the plant that was passed in.
+
+    			// store the plant that the user selected as an attribute.
+    			plant = (Plant) data.getSerializableExtra(PlantResultsActivity.PLANT_RESULT);
+
+    			/// set this plant in the TextView on the UI.
+    			txtSelectedPlant.setText(plant.toString());
+    		} else if (requestCode == CAMERA_RESULT) {
+    			// we are here because we have received a result from the camera.
+    			plantImage = (Bitmap) data.getExtras().get("data");
+
+    			imgPlant.setImageBitmap(plantImage);
+    		}
+    	}
+    }
+    
+    
+    
+    //method will be invoked when Take Photo button is clicked
+    public void onTakePhotoClicked(View v){
+    	//use an implicit intent to invoke the camera
+    	Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+    	
+    	//start this intent, and anticipate a result.
+    	startActivityForResult(cameraIntent, CAMERA_RESULT);
     }
     
     
--- src/com/plantplaces/plantplacesmobile14ss/PlantResultsActivity.java
+++ src/com/plantplaces/plantplacesmobile14ss/PlantResultsActivity.java
@@ -1,16 +1,80 @@
 package com.plantplaces.plantplacesmobile14ss;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.plantplaces.dto.Plant;
 
 public class PlantResultsActivity extends ListActivity {
+
+	public static final String PLANT_RESULT = "PLANT_RESULT";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+
+		// retrieve any data that was passed in, and was associated with the name "SEARCH_PLANT_NAME"
+		String searchTerm = getIntent().getStringExtra("SEARCH_PLANT_NAME");
+
+		//create a collection to hold the plants
+		ArrayList<Plant> allPlants = new ArrayList<Plant>();
+
+		if (searchTerm.equalsIgnoreCase("redbud")){
+			// create a new object from class Plant.
+			Plant redbud = new Plant();
+			redbud.setCommon("Redbud");
+			redbud.setGenus("Cercis");
+			redbud.setSpecies("canadensis");
+
+			//add the redbud and the paw paw to this collection
+			allPlants.add(redbud);
+		}	
+
 		
+		if (searchTerm.contains("pawpaw")){
+		//create a paw paw
+		Plant pawpaw = new Plant();
+		pawpaw.setCommon("PawPaw");
+		pawpaw.setGenus("Asimina");
+		pawpaw.setSpecies("triloba");
+		allPlants.add(pawpaw);
+		}
 		
+		//do we have an empty list?
+		if (allPlants.size()== 0){
+			Plant empty = new Plant();
+			empty.setCommon("No plants match your result. Please try again");
+            allPlants.add(empty);
+		}
+		
+		//show this collection in our list.
+		ArrayAdapter<Plant> plantAdapter = new ArrayAdapter<Plant>(this, android.R.layout.simple_list_item_1, allPlants);
+
+		setListAdapter(plantAdapter);
+	}
+		
+		@Override
+		protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+
+		// get the item that the user clicked.
+		Plant plant = (Plant) getListAdapter().getItem(position);
+
+		// put the plant in the intent which we are about to return.
+		getIntent().putExtra(PLANT_RESULT, plant);
+
+		// everything went fine.
+		setResult(RESULT_OK, getIntent());
+
+		// finish this intent.
+		finish();
 	}
 
 }
